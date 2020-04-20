fdd9aab70ef786ec86118bdd6b5f205fe1a45c3c
philippe D <philipped62@live.be>
2020-04-20 18:17:09 +0200

    modif 20042020

--- GestmetAsp/Areas/ChefEquipeZone/Controllers/JDTController.cs
+++ GestmetAsp/Areas/ChefEquipeZone/Controllers/JDTController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using UIGestmetModelsClient;
 using UIGestmetModelsClient.Services;
 
@@ -15,55 +16,44 @@ namespace GestmetAsp.Areas.ChefEquipeZone.Controllers
     public class JDTController : Controller
     {
         private IRepositoryGlobal<JournalDesTravaux> _serviceJDT;
-        //private IRepositoryGlobal<UIGestmetModelsClient.VPoste> _servicePoste;
-        //private IRepositoryGlobal<UIGestmetModelsClient.Personnel> _servicePersonnel;
-        //private IRepositoryGlobal<UIGestmetModelsClient.VChantier> _serviceChantier;
         private GetDbListView vgetlist;
         public JDTController()
         {
             _serviceJDT = new JDTRepository();
-            //_servicePoste = new VPosteRepository();
-            //_servicePersonnel = new VPersonnelRepository();
-            //_serviceChantier = new VChantierRepository();
             vgetlist = new GetDbListView();
         }
-        // GET: JDT
-       [HttpGet]
-        public ActionResult Index(bool retautreview)
+//.........................................***********INIT************...............................................
+        [HttpGet]
+        public ActionResult Init()
         {
-            if (SessionManager.SessionUser != null)
-            {
-                if(!retautreview)
-                { 
-                TempData.Add("estdisplay", "non");
-
-                }
-                /// TempData.Add("listchantier", vgetlist.GetVChantiers());
-                IEnumerable<Models.VChantier> castlistchantier = vgetlist.GetVChantiers();
-                ViewData["viewlistchantier"] = castlistchantier;
-                return View();
-            }
-            else
-            return RedirectToAction("../../Home/Index");
+            IEnumerable<Models.VChantier> listchoixchantier = vgetlist.GetVChantiers();
+            ViewData["viewlistchantier"] = listchoixchantier;
+            return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Index(DateTime? datedebut, int? choixchantier)
+        public ActionResult Init(DateTime? datedebut, int? choixchantier)
         {
             if (datedebut == null || (int)((DateTime)datedebut).DayOfWeek != 1)
             {
                 ViewBag.Message = "La date de recherche n'est pas un lundi ou n'est pas valable";
-                TempData["estdisplay"] = "non";
                 return View();
             }
-           
-            TempData["estdisplay"] = "oui";
-            IEnumerable<Models.VPoste> listposte = vgetlist.GetVPostes();
-            //new IEnumerable<Models.VPoste>();
-            //listposte = vgetlist.GetVPostes();
+            TempData["choixchantier"] = (int)choixchantier;
+            TempData["datedebut"] = (DateTime)datedebut;
+            return RedirectToAction("Index", new { choixchantier = @TempData["choixchantier"], datedebut = @TempData["datedebut"] });
+
+        }
+        //.........................................***********INDEX************...............................................       
+        [HttpGet]
+        public ActionResult Index(DateTime datedebut,int choixchantier)
+        {
             if (SessionManager.SessionUser != null)
             {
+                TempData["choixchantier"] = choixchantier;
+                TempData["datedebut"] = datedebut;
+                IEnumerable<Models.VPoste> listposte = vgetlist.GetVPostes();
                 return View("Index", _serviceJDT.GetAll().Select(s => new JDTListe()
                 {
                     Id = s.Id,
@@ -78,21 +68,22 @@ namespace GestmetAsp.Areas.ChefEquipeZone.Controllers
                     NumSemaine = s.NumSemaine,
                     EstValide = s.EstValide
                 })
-                    .Where(jdt => jdt.ChefChantierId == SessionManager.SessionUser.PersonnelId 
-                    && (listposte.FirstOrDefault(lp=>lp.IdPoste== jdt.PosteId).ChantierId==(int)choixchantier)
-                             && (jdt.DateChantier >= datedebut & jdt.DateChantier <= ((DateTime)datedebut).AddDays(6))));
-            
-            }
-            else
-                return RedirectToAction("../../Home/Index");
-        }
+                .Where(jdt => jdt.ChefChantierId == SessionManager.SessionUser.PersonnelId
+                && (listposte.FirstOrDefault(lp => lp.IdPoste == jdt.PosteId).ChantierId == (int)TempData["choixchantier"])
+                && (jdt.DateChantier >= (DateTime)TempData["datedebut"] & jdt.DateChantier <= ((DateTime)TempData["datedebut"]).AddDays(6))));
 
-        //GET: JDT/Details/5
-        public ActionResult Details(int id)
+
+            }
+            return RedirectToAction("../../Home/Index");
+        }
+//.........................................***********DETAILS************...............................................              
+        public ActionResult Details(int id, DateTime datedebut, int choixchantier)
         {
             if (SessionManager.SessionUser != null)
             {
                 JournalDesTravaux s = _serviceJDT.GetOne(id);
+                TempData["choixchantier"] = choixchantier;
+                TempData["datedebut"] = datedebut;
                 return View(new JDTListe()
                 {
                     Id = s.Id,
@@ -112,67 +103,38 @@ namespace GestmetAsp.Areas.ChefEquipeZone.Controllers
                 return RedirectToAction("../../Home/Index");
         }
 
-        // GET: JDT/Create
-        public ActionResult Create()
+//.........................................***********CREATE************...............................................       
+        [HttpGet]
+        public ActionResult Create(DateTime datedebut, int choixchantier)
         {
             if (SessionManager.SessionUser != null)
             {
+                TempData["choixchantier"] = choixchantier;
+                TempData["datedebut"] = datedebut;
                 JDTPost jdtpost = new JDTPost();
-                //GetDbListView VGetList = new GetDbListView();
-                //..........Vue POSTE...........................//
-
-                //IEnumerable<Models.VPoste> VPosteList = _servicePoste.GetAll().Select(s=>new Models.VPoste()
-                //{IdPoste=s.IdPoste,
-                //RefPoste=s.RefPoste,
-                //LibellePoste=s.LibellePoste,
-                //ChantierId=s.ChantierId,
-                //RefChantier=s.RefChantier,
-                //NomChantier=s.NomChantier,
-                //LieuChantier=s.LieuChantier
-                //});
-                jdtpost.Poste = vgetlist.GetVPostes();
-                //..........Vue PERSONNEL...........................//
-                //IEnumerable<Models.VPersonnel> VPersonnelList = _servicePersonnel.GetAll().Select(s => new Models.VPersonnel()
-                //{
-                //    Id = s.Id,
-                //    Matricule = s.Matricule,
-                //    Prenom = s.Prenom,
-                //    Nom = s.Nom,
-                //    ChefEquipe = s.ChefEquipe
-                //    });
+                jdtpost.Poste = vgetlist.GetVPostes().Where(p => p.ChantierId == (int)TempData["choixchantier"]);
                 jdtpost.Personnel = vgetlist.GetVPersonnels();
-                //................................................................................//
-                //..........Vue CHANTIER A PLACER DANS UN AUTRE CONTROLER?...........................//
-
-                //IEnumerable<Models.VChantier> VChantierList = _serviceChantier.GetAll().Select(s => new Models.VChantier()
-                //{
-                //    Id = s.Id,
-                //    Ref = s.Ref,
-                //    Nom = s.Nom,
-                //    Lieu = s.Lieu,
-                //    VNomSociete = s.VNomSociete
-
-                //});
-                //................................................................................//
-
+                
                 return View("Create", jdtpost);
             }
             else
                 return RedirectToAction("../../Home/Index");
         }
 
-        // POST: JDT/Create
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(JDTPost jdtpost)
+        public ActionResult Create(DateTime datedebut, int choixchantier, JDTPost jdtpost)
         {
             if (SessionManager.SessionUser != null)
             {
                 try
                 {
-                    _serviceJDT.Create(new JournalDesTravaux(jdtpost.PersonnelId, jdtpost.PosteId, jdtpost.DateChantier, jdtpost.HeuresProd, jdtpost.VoitPerso, jdtpost.ZoneDepl, jdtpost.ChefChantierId,SessionManager.SessionUser.PersonnelId, jdtpost.EstValide));
+                    TempData["choixchantier"] = choixchantier;
+                    TempData["datedebut"] = datedebut;
+                    _serviceJDT.Create(new JournalDesTravaux(jdtpost.PersonnelId, jdtpost.PosteId, jdtpost.DateChantier, jdtpost.HeuresProd, jdtpost.VoitPerso, jdtpost.ZoneDepl, SessionManager.SessionUser.PersonnelId, SessionManager.SessionUser.PersonnelId, jdtpost.EstValide));
 
-                    return RedirectToAction("Index");
+                    return RedirectToAction("Index", new { choixchantier = @TempData["choixchantier"], datedebut = @TempData["datedebut"] });
                 }
                 catch (Exception ex)
                 {
@@ -182,13 +144,15 @@ namespace GestmetAsp.Areas.ChefEquipeZone.Controllers
             else
                 return RedirectToAction("../../Home/Index");
         }
-
+//.........................................***********EDIT************...............................................   
         [HttpGet]
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int id,DateTime datedebut,int choixchantier)
         {
             if (SessionManager.SessionUser != null)
             {
                 JournalDesTravaux s = _serviceJDT.GetOne(id);
+                TempData["choixchantier"] = choixchantier;
+                TempData["datedebut"] = datedebut;
                 return View(new JDTPost()
                 {
                     //Id = s.Id,
@@ -202,12 +166,9 @@ namespace GestmetAsp.Areas.ChefEquipeZone.Controllers
                     Login = s.Login,
                     //NumSemaine = s.NumSemaine,
                     EstValide = s.EstValide,
-                    Poste = vgetlist.GetVPostes(),
+                    Poste = vgetlist.GetVPostes().Where(p => p.ChantierId == (int)TempData["choixchantier"]),
                     Personnel = vgetlist.GetVPersonnels()
-
-
-
-                });
+                }); 
             }
             else
                 return RedirectToAction("../../Home/Index");
@@ -215,15 +176,16 @@ namespace GestmetAsp.Areas.ChefEquipeZone.Controllers
 
         // POST: JDT/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id,  JDTPost jdtpost)
+        public ActionResult Edit(int id, DateTime datedebut, int choixchantier,  JDTPost jdtpost)
         {
             if (SessionManager.SessionUser != null)
             {
                 try
                 {
                     _serviceJDT.Update(id, new JournalDesTravaux(jdtpost.PersonnelId, jdtpost.PosteId, jdtpost.DateChantier, jdtpost.HeuresProd, jdtpost.VoitPerso, jdtpost.ZoneDepl, jdtpost.ChefChantierId,SessionManager.SessionUser.PersonnelId, jdtpost.EstValide));
-                   // bool retautreview = true;
-                    return RedirectToAction("Index",new { retautreview = true });
+                    TempData["choixchantier"] = choixchantier;
+                    TempData["datedebut"] = datedebut;
+                    return RedirectToAction("Index", new {choixchantier = @TempData["choixchantier"], datedebut = @TempData["datedebut"] });
                 }
                 catch (Exception ex)
                 {
@@ -233,35 +195,19 @@ namespace GestmetAsp.Areas.ChefEquipeZone.Controllers
             else
                 return RedirectToAction("../../Home/Index");
         }
-
-      //  [HttpDelete]
-        public ActionResult Delete(int id)
+        //.........................................***********DELETE************...............................................   
+        //  [HttpDelete]
+        public ActionResult Delete(int id, DateTime datedebut, int choixchantier)
         {
             if (SessionManager.SessionUser != null)
             {
+                TempData["choixchantier"] = choixchantier;
+                TempData["datedebut"] = datedebut;
                 _serviceJDT.Delete(id);
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", new { choixchantier = @TempData["choixchantier"], datedebut = @TempData["datedebut"] });
             }
             else
                 return RedirectToAction("../../Home/Index");
         }
-
-        // POST: JDT/Delete/5
-        //[HttpPost]
-        //public ActionResult Delete(int id, FormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add delete logic here
-
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-        
-        
     }
 }
